12905ae2d0ab1efd448e500cf74e4da41b4b43d6
mtruong68 <marytruong2020@u.northwestern.edu>
2017-11-02 10:05:33 -0500

    more login changes

--- js/login.js
+++ js/login.js
@@ -1,11 +1,11 @@
 var incorrect_pw = document.getElementById("incorrect_pw");
-var emailinput = document.getElementById("text-input-e").value;
-var pwinput = document.getElementById("text-input-p").value;
+var emailinput = document.getElementById("text-input-e");
+var pwinput = document.getElementById("text-input-p");
 
 function submitlogin() {
-	//if (emailinput == "shelter" && pwinput == "password"){
+	if ((emailinput.value == "shelter") && (pwinput.value == "password")){
 		window.location.href = "manager_overview.html";
-	//} else {
-	//	incorrect_pw.style.visibility = "visible";
-	//}
+	} else {
+		incorrect_pw.style.visibility = "visible";
+	}
 }
