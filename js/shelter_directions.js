b42f8f12dfde2384055733d795f30ccc115dfadd
mtruong68 <marytruong2020@u.northwestern.edu>
2017-11-04 12:08:55 -0500

    js for direction modes + tables

--- js/shelter_directions.js
+++ js/shelter_directions.js
@@ -1,6 +1,12 @@
 var walk = document.getElementById("walk");
 var car = document.getElementById("car");
 var transit = document.getElementById("transit");
+var map = document.getElementById("image");
+
+var walktable = document.getElementById("walktable");
+var cartable = document.getElementById("cartable");
+var transittable = document.getElementById("transittable");
+
 var buttons = [walk, car, transit];
 
 function walk_f() {
@@ -8,19 +14,37 @@ function walk_f() {
 	for (i=0; i<3; ++i){
 		buttons[i].classList.remove("on");
 	}
+
 	walk.classList.add("on");
+	map.src = "images/evanston_map_directions.PNG";
+
+	cartable.style.display = "none";
+	transittable.style.display = "none";
+	walktable.style.display = "table";
 }
 
 function car_f() {
 	for (i=0; i<3; ++i){
 		buttons[i].classList.remove("on");
 	}
+
 	car.classList.add("on");
+	map.src = "images/car_directions.PNG";
+
+	cartable.style.display = "table";
+	transittable.style.display = "none";
+	walktable.style.display = "none";
 }
 
 function transit_f() {
 	for (i=0; i<3; ++i){
 		buttons[i].classList.remove("on");
 	}
+
 	transit.classList.add("on");
+	map.src = "images/bus_directions.PNG";
+
+	cartable.style.display = "none";
+	transittable.style.display = "table";
+	walktable.style.display = "none";
 }
