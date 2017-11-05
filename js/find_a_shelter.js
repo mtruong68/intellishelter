b98aa780b5b7b5bb23bd17e1b5d2e50e50342e7f
David Wolff <davidwolff2020@u.northwestern.edu>
2017-11-05 00:39:24 -0500

    finding a shelter is now easier!

--- js/find_a_shelter.js
+++ js/find_a_shelter.js
@@ -1,6 +1,11 @@
+var userInput;
+
 $(document).ready(function() {
   console.log("ready!")
-  $("#autocomplete").click(function() {
+
+  $("#autocomplete").val(userInput);
+
+  $("#autocomplete").focus(function() {
     $( "#autocomplete" ).autocomplete({
       minLength: 0,
       source: ["Use Current Location"]
@@ -8,16 +13,17 @@ $(document).ready(function() {
   }).focus(function() {
     $(this).autocomplete("search", "");
   });
-  var shelters =`<div class='shelter' id="YWCA"><span><b>YWCA Evanston</b> - 1.7m</span><span>1215 Church St.</span></div>
-  <div class='shelter' id="Salvation"><span><b>Salvation Army Disaster</b> - 2.1m</span><span>1403 Sherman Ave.</span></div>
-  <div class='shelter' id="RedCross"><span><b>Red Cross Shelter</b> - 2.2m</span><span>623 E. McCourt</span></div>
-  <div class='shelter' id="United"><span><b>United Center</b> - 8.9m</span><span>1901 W Madison St.</span></div>
-`
+
   $("#submit").click(function() {
-    if($("#autocomplete").val() !== '') {
-      $("#autocomplete").val("2332 Campus Drive");
-      $("#image").attr('src','images/Evanston_map.png');
-      $('.search-box').append(shelters);
+    userInput = $("#autocomplete").val();
+    if(userInput !== '') {
+      if(userInput === "Use Current Location") {
+        userInput = "2332 Campus Drive"
+      }
+      window.location.href = "find_a_shelter_AFTER.html";
+    } else {
+      $('.search-box .shelter').remove();
+      $('.search-box').append(`<div class='shelter' id="Invalid"><span><b>Invalid Input. Please enter a shelter location</span></div>`);
     }
   })
 
