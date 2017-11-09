77cb2d04c83dd6c704bde98c8e12ae2d0b40645c
simonlothschutz <simon.lothschutz@gmail.com>
2017-11-08 19:16:49 -0600

    fixed null detect

--- js/manager_overview.js
+++ js/manager_overview.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
-	if(localStorage.getItem("name") != null){
+	if(localStorage.getItem("name") == null || localStorage.getItem("name") == ""){
+		$("#top_text").text("YWCA Evanston");
+	}
+	else{
 		$("#top_text").text(localStorage.getItem("name", name));
 	}
 });
