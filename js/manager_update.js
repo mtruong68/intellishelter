12cd1860c0f405a82cea8244728cbdf48956eb58
simonlothschutz <simon.lothschutz@gmail.com>
2017-11-12 22:04:36 -0600

    cleaned up manager code

--- js/manager_update.js
+++ js/manager_update.js
@@ -1,34 +1,32 @@
 $(document).ready(function(){
 
-	//if its storage exists, use that, else default
 	if(localStorage.getItem("name") != null){		
 		var name = localStorage.getItem("name");
+		var number = localStorage.getItem("number");
 		var capacity = localStorage.getItem("capacity");
 		var max_capacity = localStorage.getItem("max_capacity");
 		var additional_info = localStorage.getItem("additional_info");
-	}else{
-		var name = $("#form_name").val();
-		var capacity = $("#form_cap").val();
-		var max_capacity = $("#form_max").val();
-		var additional_info = $("#update_info").val();
-	}
-
-	//set current values set above
-	$("#form_name").val(name);
-	$("#form_cap").val(capacity);
-	$("#form_max").val(max_capacity);
-	$("#update_info").val(additional_info);
 	
+		$("#form_name").val(name);
+		$("#form_number").val(number);
+		$("#form_cap").val(capacity);
+		$("#form_max").val(max_capacity);
+		$("#update_info").val(additional_info);
+	}
+		
 	//on click, set storage values to current values
 	$("#submit_button").click(function(){
 		name = $("#form_name").val();
+		number = $("#form_number").val();
 		capacity = $("#form_cap").val();
 		max_capacity = $("#form_max").val();
 		additional_info = $("#update_info").val();
 
 		localStorage.setItem("name", name);
+		localStorage.setItem("number", number);
 		localStorage.setItem("capacity", capacity);
 		localStorage.setItem("max_capacity", max_capacity);
 		localStorage.setItem("additional_info", additional_info);
-	});	
+	});
+
 });
