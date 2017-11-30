2860b21aa6cbba641f91988d86baa8b76614e8e5
NuDrake <drakehubbard2020@u.northwestern.edu>
2017-11-29 22:53:56 -0600

    push notifications is functional, working on positioning and colors.

--- js/advanced_shelter_info.js
+++ js/advanced_shelter_info.js
@@ -6,4 +6,36 @@ $(document).ready(function(){
 		$("#capacity").text(localStorage.getItem("capacity") + " / " + localStorage.getItem("max_capacity"));
 		$("#additional_info").text(localStorage.getItem("additional_info"));
 	}
+	var push_count = 0;
+
+	$( "#nots_dialog" ).dialog({ autoOpen: false });
+	$("#nots_dialog").dialog({
+		modal: true,
+		draggable: false,
+    	resizable: false,
+    	width: 300,
+    	position: ["center center", "center center", "#notifications_button"],
+    	buttons: { "I Understand": function() {
+            $(this).dialog("close");
+        	}
+    	}
+	});
+
+	$( "#notifications_button" ).click(function() {
+		if(push_count % 2 == 0){
+			document.getElementById("nots_dialog").innerHTML = "Do you want to receive push notifications from this shelter?";
+			document.getElementById("notifications_button").innerHTML = "Disable Push Notificaions";
+  			$( "#nots_dialog" ).dialog( "open" );
+  			push_count++;
+  		}
+
+  		else {
+  			document.getElementById("nots_dialog").innerHTML = "Do you want to stop receiving push notifications from this shelter?";
+  			document.getElementById("notifications_button").innerHTML = "Receive Push Notificaions";
+  			$( "#nots_dialog" ).dialog( "open" );
+  			push_count++;
+  		}
+	});
 });
+
+
