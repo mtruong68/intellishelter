4a1fae985e8840edf78a3d41bca77aec362dd30a
NuDrake <drakehubbard2020@u.northwestern.edu>
2017-11-30 10:51:41 -0600

    added scrolling functionality to advanced shelter info.  Fixed line breaks when updated from manager overview.

--- js/advanced_shelter_info.js
+++ js/advanced_shelter_info.js
@@ -37,6 +37,14 @@ $(document).ready(function(){
   			push_count++;
   		}
 	});
+
+	$( '#screen' ).on( 'mousewheel DOMMouseScroll', function ( e ) {
+    var e0 = e.originalEvent,
+        delta = e0.wheelDelta || -e0.detail;
+
+    this.scrollTop += ( delta < 0 ? 1 : -1 ) * 5;
+    e.preventDefault();
+	});
 });
 
 
